b459b917fd79ad368240a63e59d546c7cc5e1ba0
Gus <6810110485@psu.ac.th>
2025-12-19 00:15:53 +0700

    Assignment2-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import './App.css'
 import axios from 'axios'
-import { useState } from 'react';
-import LoginScreen from './LoginScreen';
-import BookScreen from './BookScreen';
-
+import { useState } from 'react'
+import LoginScreen from './LoginScreen'
+import BookScreen from './BookScreen'
 
 axios.defaults.baseURL = "http://localhost:3000"
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const handleLoginSuccess = () => {setIsAuthenticated(true)}
 
-  return (
-    <>
-      {isAuthenticated ? <BookScreen/> : <LoginScreen onLoginSuccess={handleLoginSuccess}/>}
-    </>
-  )
+  const handleLoginSuccess = () => {
+    setIsAuthenticated(true)
+  }
+
+  return isAuthenticated
+    ? <BookScreen />
+    : (
+      <LoginScreen onLoginSuccess={handleLoginSuccess} />
+    )
 }
 
 export default App
